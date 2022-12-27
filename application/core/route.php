03a6284c029fce45ef52406b9ed03301e856a348
Maxwellsol <maxwellsollow@gmail.com>
2022-12-27 10:29:44 +0400

    add error404

--- application/core/route.php
+++ application/core/route.php
@@ -6,55 +6,43 @@ class Route
 
     static function start()
     {
-        // контроллер и действие по умолчанию
         $controller_name = 'User';
         $action_name = 'index';
 
         $routes = explode('/', $_SERVER['REQUEST_URI']);
 
 
-
-        if ( !empty($routes[1]))
-        {
+        if (!empty($routes[1])) {
             $controller_name = $routes[1];
         }
 
-        if ( !empty($routes[2]) )
-        {
+        if (!empty($routes[2])) {
             $action_name = $routes[2];
         }
-        $model_name = 'Model_'.$controller_name;
-        $controller_name = 'Controller_'.$controller_name;
-        $action_name = 'action_'.$action_name;
-        $model_file = strtolower($model_name).'.php';
-        $model_path = "application/models/".$model_file;
-        if(file_exists($model_path))
-        {
-            include "application/models/".$model_file;
+        $model_name = 'Model_' . $controller_name;
+        $controller_name = 'Controller_' . $controller_name;
+        $action_name = 'action_' . $action_name;
+        $model_file = strtolower($model_name) . '.php';
+        $model_path = "application/models/" . $model_file;
+        if (file_exists($model_path)) {
+            include "application/models/" . $model_file;
         }
 
-        $controller_file = strtolower($controller_name).'.php';
-        $controller_path = "application/controllers/".$controller_file;
-        if(file_exists($controller_path))
-        {
-            include "application/controllers/".$controller_file;
+        $controller_file = strtolower($controller_name) . '.php';
+        $controller_path = "application/controllers/" . $controller_file;
+        if (file_exists($controller_path)) {
+            include "application/controllers/" . $controller_file;
+        } else {
+            Route::ErrorPage404();
         }
-        else
-        {
-            //Route::ErrorPage404();
-        }
-
-        // создаем контроллер
+        
         $controller = new $controller_name;
         $action = $action_name;
 
-        if(method_exists($controller, $action))
-        {
+        if (method_exists($controller, $action)) {
             $controller->$action();
-        }
-        else
-        {
-           // Route::ErrorPage404();
+        } else {
+            Route::ErrorPage404();
         }
 
     }
@@ -62,10 +50,10 @@ class Route
 
     function ErrorPage404()
     {
-        $host = 'http://'.$_SERVER['HTTP_HOST'].'/';
+        $host = 'http://' . $_SERVER['HTTP_HOST'] . '/';
         header('HTTP/1.1 404 Not Found');
         header("Status: 404 Not Found");
-        header('Location:'.$host.'404');
+        header('Location:' . $host . '404');
     }
 
 }
